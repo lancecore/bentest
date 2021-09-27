4eae496795706bef7cac5dea12c5a5c8d938f7ae
Lance Boer <lance@Lances-MacBook-Pro-2.local>
2021-09-27 14:35:19 -0700

    Sept 27 changes

--- index-ben.php
+++ index-ben.php
@@ -23,20 +23,15 @@
           if ( $loop->have_posts() ) :
             while ( $loop->have_posts() ) : $loop->the_post();
           ?>
-          //Look here!
+          
           <tr>
-            <td><?php if ( get_field('lead_status') ) :
-              $lead_status = get_field('lead_status');
-              foreach ( $lead_status as $lead_stat ) :
-            ?>
-              <span class="<?php echo $lead_stat->slug; ?>">
-                <?php echo $lead_stat->name; ?>
+            <td><?php if ( get_field('lead_status') ) : ?>
+              <span class="<?php echo get_field('lead_status')->slug; ?>">
+                <?php echo get_field('lead_status')->name; endif; ?>
               </span>
-              <?php endforeach; endif; ?>
               </td>
-            //The end
-            <td><?php if ( get_field('client_name') ) : the_field('client_name'); endif; ?></td>
-            <td><?php if ( get_field('meeting_time') ) : the_field('meeting_time'); endif; ?></td>
+            <td><?php if ( get_field('clients_name') ) : the_field('clients_name'); endif; ?></td>
+            <td><?php if ( get_field('meeting_timedate') ) : the_field('meeting_timedate'); endif; ?></td>
             <td><a href="<?php the_permalink(); ?>">View this one</a></td>
           </tr>
           
