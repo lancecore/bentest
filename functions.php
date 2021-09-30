ef9972dd0b2c63666e7f4864e407d54a7ea1a3f8
Lance Boer <lance@Lances-MacBook-Pro-2.local>
2021-09-30 14:55:29 -0700

    Email working

--- functions.php
+++ functions.php
@@ -5,3 +5,23 @@ if (!defined('ABSPATH')) {
 require get_template_directory() . '/functions/setup.php';
 require get_template_directory() . '/functions/enqueues.php';
 require get_template_directory() . '/functions/navbar.php';
+
+add_action('acf/save_post', 'my_acf_save_post');
+function my_acf_save_post( $post_id ) {
+
+    // Get newly saved values.
+    $values = get_fields( $post_id );
+
+    // Check the new value of a specific field.
+    $assigned = get_field('assigned', $post_id);
+    if( $assigned ) {
+      foreach( $assigned as $user ):
+        $to = $user[user_email];
+        $subject = "Lead up date";
+        $message = "You have been assigned to a lead";
+        $headers = "test";
+        wp_mail( $to, $subject, $message, $headers );
+      endforeach;
+
+    }
+}
